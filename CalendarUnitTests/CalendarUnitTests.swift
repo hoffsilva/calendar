f22045bf15bce58c546147c6b549127a3e553da0
Hoff Silva <hoff.silva@ebanx.com>
2021-06-03 19:49:20 -0300

    Add unit test target

---
+++ CalendarUnitTests/CalendarUnitTests.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarUnitTests.swift
+//  CalendarUnitTests
+//
+//  Created by Hoff Silva on 03/06/21.
+//
+
+import XCTest
+@testable import Calendar
+
+class CalendarUnitTests: XCTestCase {
+
+    
+    
+}
