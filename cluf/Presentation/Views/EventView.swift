a3124645805a885f401af3b3886b41abf0a14f46
Hoff Silva <hoff.silva@ebanx.com>
2021-06-15 22:37:12 -0300

    UI Improvements

--- cluf/Presentation/Views/EventView.swift
+++ cluf/Presentation/Views/EventView.swift
@@ -13,24 +13,29 @@ struct EventView: View {
     
     var body: some View {
         HStack {
-            Text("\(event.startDate.getDay())")
-                .font(.rubikBold(90))
+            Text(event.startDate.getDay())
+                .font(.rubikBold(80))
+                .multilineTextAlignment(.center)
                 .rotationEffect(.degrees(270))
+                .frame(maxWidth: .infinity, alignment: .leading)
             Image("vertical-divider")
-            HStack {
-                Image("bullet")
-                VStack(alignment: .leading) {
+                .frame(maxWidth: 1, alignment: .leading)
+            HStack(alignment: .top) {
+                Image("bullet").padding([.top], 6)
+                VStack(alignment: .leading, spacing: 8) {
                     Text("event.status")
                         .font(.rubikRegular(16))
                         .foregroundColor(.gray)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     Text(event.startDate.getHour())
                         .font(.rubikBold(20))
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     Text(event.title)
                         .font(.rubikRegular(16))
                         .foregroundColor(.gray)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
             }
-            
             Image("righ-arrow")
         }
     }
