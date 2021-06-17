4b20c7e9d2c8789b3f16df944675f6950f75ced5
Hoff Silva <hoff.silva@ebanx.com>
2021-06-17 20:31:20 -0300

    Added Color extension

---
+++ cluf/Presentation/Extension+Color.swift
@@ -0,0 +1,24 @@
+//
+//  Extension+Color.swift
+//  cluf
+//
+//  Created by Hoff Silva on 17/06/21.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    var clufRed: Color {
+        Color(red: 0.942, green: 0.153, blue: 0.153)
+    }
+    
+    var clufGray: Color {
+        Color(red: 0.8, green: 0.8, blue: 0.8)
+    }
+    
+    var clugDarkGray: Color {
+        Color(red: 0.565, green: 0.565, blue: 0.565)
+    }
+    
+}
