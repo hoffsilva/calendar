d1408dfb631aaf9c7f0d8b2d351b89698e2735af
Hoff Silva <hoff.silva@ebanx.com>
2021-07-12 21:17:14 -0300

    Rename project from cluf to timetable

---
+++ timetable/timetableApp.swift
@@ -0,0 +1,18 @@
+//
+//  clufApp.swift
+//  cluf
+//
+//  Created by Hoff Silva on 14/06/21.
+//
+
+import SwiftUI
+
+@main
+struct timetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
