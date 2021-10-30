88647ee724954cfa69e2830ce217e218ea9b0bd4
Hoff Silva <hoff.silva@ebanx.com>
2021-10-30 11:12:30 -0400

    adding table view controller

--- timetable/Presentation/Views/EventView.swift
+++ timetable/Presentation/Views/EventView.swift
@@ -9,21 +9,8 @@ import UIKit
 
 final class EventView: UIViewController {
     
-    private lazy var label: UILabel = {
-        let label = UILabel()
-        label.font = .rubikBold(16)
-        label.text = "Teste"
-        label.tintColor = .timetableLabelTint
-        label.prepareForConstraints()
-        return label
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBackground
-        self.view.addSubview(label)
-        self.label.centerVertically()
-        self.label.centerHorizontally()
     }
     
 }
