7d94cddafef2784f44da10ac852927a71b08f2e3
Hoff Silva <hoff.silva@ebanx.com>
2021-11-28 09:11:54 -0500

    Adding event list view

---
+++ timetable/Presentation/Views/EventListView.swift
@@ -0,0 +1,29 @@
+//
+//  EventListView.swift
+//  timetable
+//
+//  Created by Hoff Silva on 28/11/21.
+//
+
+import UIKit
+
+class EventListView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
