bc539a8fe7623da4d2948dd791cbb8c21b041ee5
Hoff Silva <hoff.silva@ebanx.com>
2021-12-21 19:59:16 -0500

    Added new logic to get the events.

--- timetable/Data/EventsDataSourceImp.swift
+++ timetable/Data/EventsDataSourceImp.swift
@@ -31,43 +31,78 @@ final class EventsDataSourceImp: EventsDataSource {
     
     func getEvents(from: Int, completion: @escaping ((Result<[Month], Error>) -> Void)) {
         
-        guard
-            let startDate = dateFormatter.date(from: "01/01/\(from)"),
-            let endDate = dateFormatter.date(from: "31/12/\(from)")
-        else { return completion(.failure(NSError(domain: "", code: 0, userInfo: nil))) }
+       let date = Date()
+        guard let next = Calendar(identifier: .gregorian).date(byAdding: .year, value: 1, to: date) else { return }
         
-        let predicate = store.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        let predicate = store.predicateForEvents(withStart: date, end: next, calendars: nil)
         let events = store.events(matching: predicate)
         
         completion(.success(mapEvent(events: events)))
     }
     
+    private func populateInfoOf(month: inout Month, with event: EKEvent) {
+        month.name = event.startDate.getLongMonth()
+        let newEvent = Event(
+            startDate: event.startDate,
+            endDate: event.endDate,
+            isAllDay: event.isAllDay,
+            location: event.location,
+            title: event.title,
+            year: event.startDate.getYear(),
+            day: event.startDate.getIntDay()
+        )
+        month.events.append(newEvent)
+    }
+    
     private func mapEvent(events: [EKEvent]) -> [Month] {
         
+        var jan = Month(name: "", events: [Event](), month: .jan)
+        var fev = Month(name: "", events: [Event](), month: .fev)
+        var mar = Month(name: "", events: [Event](), month: .mar)
+        var apr = Month(name: "", events: [Event](), month: .apr)
+        var may = Month(name: "", events: [Event](), month: .may)
+        var jun = Month(name: "", events: [Event](), month: .jun)
+        var jul = Month(name: "", events: [Event](), month: .jul)
+        var aug = Month(name: "", events: [Event](), month: .aug)
+        var sep = Month(name: "", events: [Event](), month: .sep)
+        var oct = Month(name: "", events: [Event](), month: .oct)
+        var nov = Month(name: "", events: [Event](), month: .nov)
+        var dec = Month(name: "", events: [Event](), month: .dec)
+        
+        
+        
         for event in events {
-            switch event.startDate.getMonth() {
+            switch event.startDate.getShortMonth() {
             case Months.jan.rawValue:
-                print()
+                populateInfoOf(month: &jan, with: event)
+            case Months.fev.rawValue:
+                populateInfoOf(month: &fev, with: event)
+            case Months.mar.rawValue:
+                populateInfoOf(month: &mar, with: event)
+            case Months.apr.rawValue:
+                populateInfoOf(month: &apr, with: event)
+            case Months.may.rawValue:
+                populateInfoOf(month: &may, with: event)
+            case Months.jun.rawValue:
+                populateInfoOf(month: &jun, with: event)
+            case Months.jul.rawValue:
+                populateInfoOf(month: &jul, with: event)
+            case Months.aug.rawValue:
+                populateInfoOf(month: &aug, with: event)
+            case Months.sep.rawValue:
+                populateInfoOf(month: &sep, with: event)
+            case Months.oct.rawValue:
+                populateInfoOf(month: &oct, with: event)
+            case Months.nov.rawValue:
+                populateInfoOf(month: &nov, with: event)
+            case Months.dec.rawValue:
+                populateInfoOf(month: &dec, with: event)
             default:
-                print()
+                ()
             }
         }
-        
-//        events.map { event in
-//            Event(
-//                startDate: event.startDate,
-//                endDate: event.endDate,
-//                isAllDay: event.isAllDay,
-//                location: Location(
-//                    latitude: event.structuredLocation?.geoLocation?.coordinate.latitude ?? 0,
-//                    longitude: event.structuredLocation?.geoLocation?.coordinate.longitude ?? 0
-//                ),
-//                title: event.title,
-//                year: event.startDate.getYear(),
-//                day: event.startDate.getIntDay()
-//            )
-//        }
-       return [Month]()
+        let months = [jan, fev, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
+        return months.filter { !$0.name.isEmpty }
     }
     
 }
