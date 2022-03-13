1194e5be6141a9f009a454a1b203f239c7f8842c
Hoff Silva <hoff.silva@ebanx.com>
2022-03-13 09:24:57 -0400

    Added separator view

--- UI/Views/DetailDayEventCell.swift
+++ UI/Views/DetailDayEventCell.swift
@@ -12,10 +12,12 @@ class DetailDayEventCell: UITableViewCell {
     
     @IBOutlet weak var hourLabel: UILabel!
     @IBOutlet weak var hourEventsCollectionView: UICollectionView!
+    @IBOutlet weak var sparatorView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         hourLabel.font = .rubikRegular(16)
+        sparatorView.backgroundColor = .timetableGray
     }
     
     public func setupData(_ data: Hour) {
