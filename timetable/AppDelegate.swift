a35d0a87811c41cd57c4a421867ddc79bbfbffb4
Hoff Silva <hoff.henry@gmail.com>
2022-06-01 17:47:38 -0400

    Remove unnecesary code.

--- timetable/AppDelegate.swift
+++ timetable/AppDelegate.swift
@@ -8,17 +8,5 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        try! FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
-        
-        do {
-            sleep(5)
-        }
-        
-        return true
-    }
-    
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
 
