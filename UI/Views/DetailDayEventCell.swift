eda705d119932e5c9aa0ab7b80995fa2ed8f413a
Hoff Silva <hoff.henry@gmail.com>
2022-06-13 23:15:08 -0400

    Convert DetailDailEventCell to view code.

--- UI/Views/DetailDayEventCell.swift
+++ UI/Views/DetailDayEventCell.swift
@@ -10,23 +10,56 @@ import Domain
 
 class DetailDayEventCell: UITableViewCell {
     
-    @IBOutlet weak var hourLabel: UILabel!
-    @IBOutlet weak var hourEventsCollectionView: UICollectionView!
-    @IBOutlet weak var sparatorView: UIView!
+    private lazy var hourLabel: UILabel = {
+        let label = UILabel()
+        label.prepareForConstraints()
+        label.font = .rubikRegular(16)
+        return label
+    }()
     
-    private lazy var dataSource = makeDataSource()
+    private lazy var hourEventsCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.prepareForConstraints()
+        return collectionView
+    }()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        hourLabel.font = .rubikRegular(16)
-        sparatorView.backgroundColor = .timetableGray
-        self.backgroundColor = .clear
-        self.hourEventsCollectionView.backgroundColor = .clear
+    private lazy var separatorView: UIView = {
+        let view = UIView()
+        view.prepareForConstraints()
+        view.backgroundColor = .timetableGray
+        return view
+    }()
+    
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Event>?
+
+    private func setupViewsHierarchy() {
+        self.contentView.addSubview(hourLabel)
+        self.contentView.addSubview(hourEventsCollectionView)
+        self.contentView.addSubview(separatorView)
+    }
+    
+    private func setupViewsConstraints() {
+        hourLabel.centerVertically()
+        hourLabel.pinLeft()
+        hourEventsCollectionView.pinLeftInRelation(to: hourLabel.rightAnchor)
+        hourEventsCollectionView.pinRight(24)
+        hourEventsCollectionView.pinTop()
+        hourEventsCollectionView.pinBottomInRelation(to: separatorView.topAnchor)
+        separatorView.pinLeft()
+        separatorView.pinRight()
+        separatorView.pinBottom()
+        separatorView.height(with: 1.5)
     }
     
     public func setupData(_ data: Hour) {
-        hourLabel.text = data.name.lowercased()
         registerCell()
+        self.backgroundColor = .clear
+        self.hourEventsCollectionView.backgroundColor = .clear
+        self.hourEventsCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        setupViewsHierarchy()
+        setupViewsConstraints()
+        dataSource = makeDataSource()
+        hourLabel.text = data.name.lowercased()
         updateDataSource(listOfHour: data.events)
     }
     
@@ -34,7 +67,7 @@ class DetailDayEventCell: UITableViewCell {
         var snapshot = NSDiffableDataSourceSnapshot<Int, Event>()
         snapshot.appendSections([1])
         snapshot.appendItems(listOfHour, toSection: 1)
-        dataSource.apply(snapshot)
+        dataSource?.apply(snapshot)
     }
     
     private func registerCell() {
@@ -43,11 +76,43 @@ class DetailDayEventCell: UITableViewCell {
     }
     
     private func makeDataSource() -> UICollectionViewDiffableDataSource<Int, Event> {
-        UICollectionViewDiffableDataSource(collectionView: hourEventsCollectionView) { tableView, indexPath, itemIdentifier in
-            guard let cell = tableView.dequeueReusableCell(withReuseIdentifier: String(describing: DetailEventCell.self), for: indexPath) as? DetailEventCell else { return UICollectionViewCell() }
-            cell.setupData(itemIdentifier)
-            return cell
+        
+        let cellRegistration = UICollectionView.CellRegistration
+        <DetailEventCell, Event> { (cell, indexPath, item) in
+            cell.setupData(item)
         }
+        
+        return UICollectionViewDiffableDataSource(collectionView: hourEventsCollectionView) { tableView, indexPath, itemIdentifier in
+            tableView
+                .dequeueConfiguredReusableCell(
+                    using: cellRegistration,
+                    for: indexPath,
+                    item: itemIdentifier
+                )
+        }
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
+            let contentSize = layoutEnvironment.container.effectiveContentSize
+            let columns = contentSize.width > 800 ? 3 : 2
+            let spacing = CGFloat(10)
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .absolute(32))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            group.interItemSpacing = .fixed(spacing)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = spacing
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+            return section
+        }
+        return layout
     }
     
 }
