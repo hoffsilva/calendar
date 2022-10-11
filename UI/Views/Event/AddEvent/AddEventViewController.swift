9692d0e177d6d29efebe963e6ee53cdb9bbfb62d
Hoff Silva <hoff.henry@gmail.com>
2022-10-11 14:41:30 -0400

    Added functions to format the objects of the add event screen.

--- UI/Views/Event/AddEvent/AddEventViewController.swift
+++ UI/Views/Event/AddEvent/AddEventViewController.swift
@@ -13,7 +13,7 @@ class AddEventViewController: UIViewController {
     
     @Injected var daysEventsViewModel: DaysEventsViewModel
     
-    @IBOutlet weak var backButton: UILabel!
+    @IBOutlet weak var cancelButton: UILabel!
     @IBOutlet weak var saveEventButton: UILabel!
     @IBOutlet weak var numberOfTheDayLabel: UILabel!
     @IBOutlet weak var nameOfMonthLabel: UILabel!
@@ -41,6 +41,80 @@ class AddEventViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateNumberOfDay(with: 24)
+        animateNameOfMonth(with: 8)
+    }
+    
+    private func animateNumberOfDay(with constant: CGFloat) {
+        self.numberOfDayLeadingConstraint.constant = constant
+        UIView.animate(withDuration: 0.3, delay: 0.13) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func animateNameOfMonth(with constant: CGFloat) {
+        self.nameOfMonthLeadingConstraint.constant = constant
+        UIView.animate(withDuration: 0.1, delay: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func setLabelStrings() {
+        cancelButton.text = Localizable.backButtonTitle()
+        saveEventButton.text = Localizable.addEventButtonTitle()
+    }
+    
+    private func setupStyle() {
+        setupLabelFont()
+        setupLabelColor()
+        setupBackButton()
+        setupSaveEventButton()
+        separatorView.backgroundColor = .timetableGray
+        self.view.backgroundColor = .timetableSystemBackgroundColor
+    }
+    
+    private func setupLabelFont() {
+        cancelButton.font = .rubikBold(16)
+        saveEventButton.font = .rubikBold(16)
+        nameOfDayLabel.font = .rubikRegular(12)
+        numberOfTheDayLabel.font = .rubikBold(40)
+        nameOfMonthLabel.font = .rubikBold(40)
+    }
+    
+    private func setupLabelColor() {
+        cancelButton.textColor = .timetableRed
+        saveEventButton.textColor = .timetableRed
+        nameOfDayLabel.textColor = .timetableText
+        numberOfTheDayLabel.textColor = .timetableText
+        nameOfMonthLabel.textColor = .timetableGray
+    }
+    
+    private func setupBackButton() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapOnCancelButton))
+        cancelButton.isUserInteractionEnabled = true
+        cancelButton.addGestureRecognizer(tap)
+    }
+    
+    private func setupSaveEventButton() {
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(didTapOnBackButton))
+//        addEventButton.isUserInteractionEnabled = true
+//        addEventButton.addGestureRecognizer(tap)
+//        addEventButton.addTarget(self, action: #selector(didTapOnCloseButton), for: .touchUpInside)
+    }
+    
+    @objc private func didTapOnCancelButton() {
+        animateNumberOfDay(with: -58)
+        animateNameOfMonth(with: 36)
+        UIView.animate(withDuration: 0.6, delay: 0, options: .curveLinear) {
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
 
 }
