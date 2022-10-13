2f46ced6284b2ff19dc919b1cbef71ceeb905dc9
Hoff Silva <hoff.henry@gmail.com>
2022-10-13 15:40:27 -0400

    Create AddEventViewModel.

--- Presentation/AddEventViewModel.swift
+++ Presentation/AddEventViewModel.swift
@@ -8,30 +8,44 @@
 
 import Foundation
 import Domain
+import Data
 
 public final class AddEventViewModel: ObservableObject {
     
-    private var addEventUseCase: AddEventUseCase
+    private let addEventUseCase: AddEventUseCase
+    private let currentDate: Date
     
     public var didGetErrorMessage: ((String)->Void)?
-    public var months: [Month]?
+    public var numberOfDay: ((String)->Void)?
+    public var nameOfMonth: ((String)->Void)?
+    public var nameOfDay: ((String)->Void)?
+    public var selectedDate: ((Date)->Void)?
     
     public init(
-        addEventUseCase: AddEventUseCase
+        addEventUseCase: AddEventUseCase,
+        currentDate: Date
     ) {
-            self.addEventUseCase = addEventUseCase
-        }
-    
-    public func requestAccess(_ event: Event) {
-        addEventUseCase.addEvent(event, completion: { result in
-            switch result {
-            case .success(let eventIdentifier):
-                print(eventIdentifier) //TODO: 
-            case .failure(let error):
-                self.didGetErrorMessage?(error.localizedDescription)
-            }
-        })
+        self.addEventUseCase = addEventUseCase
+        self.currentDate = currentDate
     }
+    
+    public func loadCurrentDate() {
+        numberOfDay?(currentDate.getDay())
+        nameOfMonth?(currentDate.getLongMonth())
+        nameOfDay?(currentDate.getDayOfWeek())
+        selectedDate?(currentDate)
+    }
+    
+    //    public func loadData() {
+    //        addEventUseCase.addEvent(event, completion: { result in
+    //            switch result {
+    //            case .success(let eventIdentifier):
+    //                print(eventIdentifier) //TODO:
+    //            case .failure(let error):
+    //                self.didGetErrorMessage?(error.localizedDescription)
+    //            }
+    //        })
+    //    }
     
 }
 
